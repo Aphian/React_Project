f1a3eb313da50d4c798c0e72322694975a343bd9
Aphian <rsw7876@naver.com>
2023-12-05 21:49:28 +0900

    REST API Setting

--- backend/fine_tuning_chatbot/urls.py
+++ backend/fine_tuning_chatbot/urls.py
@@ -1,5 +1,16 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+# from . import views
+from . views import (
+    FineTunedModelViewSet,
+    TrainingDataViewSet
+)
+
+router = DefaultRouter()
+router.register(r'fine_tuned_models', FineTunedModelViewSet)
+router.register(r'training_data', TrainingDataViewSet)
+
 urlpatterns = [
-    path('hello/',views.hello_world),
+    # path('hello/',views.hello_world),
+    path('', include(router.urls)),
 ]
--- backend/fine_tuning_chatbot/views.py
+++ backend/fine_tuning_chatbot/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from django.conf import settings
+from django.core.files.storage import default_storage
+
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from . models import FineTunedModel, TrainingData
+from . serializers import FineTunedModelSerializer, TrainingDataSerializer
 
 # Create your views here.
 
 @api_view(['GET'])
 def hello_world(request):
     return Response('Hello, World!')
+
+class FineTunedModelViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    queryset = FineTunedModel.objects.all()
+    serializer_class = FineTunedModelSerializer
+
+class TrainingDataViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    queryset = TrainingData.objects.all()
+    serializer_class = TrainingDataSerializer
