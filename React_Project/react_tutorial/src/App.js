cd9e9840f530d9a199b1d62d604207c1eac3ec6a
Aphian <rsw7876@naver.com>
2024-04-12 22:23:44 +0900

    react

--- React_Project/react_tutorial/src/App.js
+++ React_Project/react_tutorial/src/App.js
@@ -1,5 +1,8 @@
 // export 키워드 사용으로 이 파일 외부에서 이 기능에 엑세스를 허용.
 // default 키워드 사용하는 다른 파일에 이것이 파일의 주요 기능임을 알림.
 export default function Square() {
+
+  // button 반환
+  // return 뒤에 오는 모든 항목이 함수 호출자에게 값으로 반환됨
   return <button className="square">X</button>;
 }
--- React_Project/react_tutorial/src/index.js
+++ React_Project/react_tutorial/src/index.js
@@ -1,3 +1,5 @@
+// 파일에서 생성한 구성요소와 웹 브라우저 사이의 다리 역할
+
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
