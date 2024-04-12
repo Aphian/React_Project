d902271de26760459a58a74d96f5f03778a8ceee
Aphian <rsw7876@naver.com>
2024-04-12 22:26:32 +0900

    react

--- React_Project/react_tutorial/src/App.js
+++ React_Project/react_tutorial/src/App.js
@@ -4,5 +4,16 @@ export default function Square() {
 
   // button 반환
   // return 뒤에 오는 모든 항목이 함수 호출자에게 값으로 반환됨
-  return <button className="square">X</button>;
+  // return <button className="square">X</button>;
+
+  // JSX는 여러 개의 인접한 요소가 아닌 단일 요소만 반환해야함.
+  // 해결방법 : <> ... </> 사용해야함
+  // return <button className="square">X</button><button className="square">X</button>
+
+  return (
+    <>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </>
+  )
 }
