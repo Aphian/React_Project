d5c520d6693f0a38a1f796d9c3e303c0211c77d1
Sang <rsw7876@naver.com>
2024-04-19 22:18:16 +0900

    react

--- React_Project/react_start/index.js
+++ React_Project/react_start/index.js
@@ -1,6 +1,12 @@
 import {createRoot} from 'react-dom/client'
 
-document.body.innerHTML = '<div id="app"></div>';
+// document.body.innerHTML = '<div id="app"></div>';
+function NavigationBar() {
+    return <h1>Hello World</h1>;
+}
 
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello World</h1>);
+// const root = createRoot(document.getElementById('app'));
+const domNode = document.getElementById('navigation');
+const root = createRoot(domNode);
+// root.render(<h1>Hello World</h1>);
+root.render(<NavigationBar />);
